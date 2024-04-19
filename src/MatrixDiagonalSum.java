9f2cb915afbeb9d2d552963475c3185b89b576ec
Arpan Nandi <arpan_nandi@epam.com>
2024-04-20 01:43:03 +0530

    Matrix Diagonal Sum

---
+++ src/MatrixDiagonalSum.java
@@ -0,0 +1,20 @@
+public class MatrixDiagonalSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int diagonalSum(int[][] mat) {
+	    final int n = mat.length;
+	    int ans = 0;
+
+	    for (int i = 0; i < n; ++i) {
+	      ans += mat[i][i];
+	      ans += mat[n - 1 - i][i];
+	    }
+
+	    return n % 2 == 0 ? ans : ans - mat[n / 2][n / 2];
+	  }
+
+}
