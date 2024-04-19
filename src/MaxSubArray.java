c7e73720620f46b395331ee6e966cbf43a8126fd
Arpan Nandi <arpan_nandi@epam.com>
2024-04-20 03:42:07 +0530

    max sub array and spiral matrix

---
+++ src/MaxSubArray.java
@@ -0,0 +1,22 @@
+
+public class MaxSubArray {
+	public int maxSubArray(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        
+        int maxSum = nums[0];
+        int currSum = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(currSum<0)
+                currSum=0;
+            currSum = currSum+nums[i];
+            maxSum = Math.max(maxSum,currSum);
+        }
+        return maxSum;
+
+
+    }
+
+}
